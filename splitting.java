e4bd643083c1ad31d8646bd2e7e22e4b213cee59
Anjali Patel <anjlip2112@gmail.com>
2021-07-31 16:12:19 +0530

    Update splitting.java

--- splitting.java
+++ splitting.java
@@ -1,1 +1,16 @@
+package com.company;
+
+public class splitting {
+    public static void main(String[] args) {
+        String str = " Earth, Mars, Jupiter, Neptune, Saturn";
+
+        String planets[] = str.split(",");
+
+        System.out.println(planets[1]);
+
+        for(String vals : planets) {
+            System.out.println(vals);
+        }
+    }
+}
 
