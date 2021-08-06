3c0f42578eebb65cf295b3bb37869146beb52727
Anjali Patel <anjlip2112@gmail.com>
2021-08-06 20:46:56 +0530

    Update userDefinedException.java

--- userDefinedException.java
+++ userDefinedException.java
@@ -1,1 +1,29 @@
 
+package com.company;
+
+import java.util.Scanner;
+
+public class userdefinedException {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your GRE score: ");
+
+        try{
+        int score = sc.nextInt();
+        if(score>=260)
+            System.out.println("You are eligible");
+        else
+            throw new MyException("Score cannot be below 260");
+        }
+        catch (MyException e){
+            System.out.println(e);
+        }
+
+    }
+}
+
+class MyException extends Exception{
+    public MyException(String msg){
+super(msg);
+    }
+}
