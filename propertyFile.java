c83602a3ab65606adb4d0862d2f706cbe5ad7cb9
Anjali Patel <anjlip2112@gmail.com>
2021-08-16 17:54:23 +0530

    Add files via upload

---
+++ propertyFile.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
+public class propertyFile {
+    public static void main(String[] args) throws Exception {
+        Properties pt = new Properties();
+
+        OutputStream os = new FileOutputStream("data.properties");
+        InputStream is = new FileInputStream("data.properties");
+        pt.load(is);
+
+        pt.setProperty("uname", "abcde");
+        pt.setProperty("url", "www.abcde.com");
+        pt.setProperty("password", "1234567");
+
+        pt.store(os,null);
+
+        System.out.println(pt.getProperty("uname"));
+        System.out.println(pt.getProperty("password"));
+
+    }
+}
